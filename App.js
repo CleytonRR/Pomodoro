471ceafc613cbe1026fa648ae53877c6a4b3a0df
cleytonRR <cleytonrodrigues69@live.com>
2020-03-10 22:01:07 -0300

    Configure base for container of aplication

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#d9bbf9',
     alignItems: 'center',
     justifyContent: 'center',
   },
